8d4d0253846eb4721ce7d85b360d86f918dfbcd7
ahsankhan <khanahsan695@gmail.com>
2025-04-11 15:35:51 +0500

    some changes

--- lib/viewmodels/providers/favorites_provider.dart
+++ lib/viewmodels/providers/favorites_provider.dart
@@ -8,22 +8,36 @@ class FavoritesProvider with ChangeNotifier {
   List<Movie> _favoriteMovies = [];
 
   FavoritesProvider() {
-    _loadFavorites(); // Load favorites when the provider is created
+    _initFavorites();
   }
 
   List<Movie> get favoriteMovies => _favoriteMovies;
 
-  void _loadFavorites() async {
+  Future<void> _initFavorites() async {
+    final prefs = await SharedPreferences.getInstance();
+
+    final bool isFirstLaunch = prefs.getBool('isFirstLaunch') ?? true;
+    if (isFirstLaunch) {
+      await prefs.remove('favoriteMovies');
+      await prefs.setBool('isFirstLaunch', false); // Mark that the app has been launched
+    }
+
+    await _loadFavorites();
+  }
+
+  Future<void> _loadFavorites() async {
     final prefs = await SharedPreferences.getInstance();
     final String? favoritesJson = prefs.getString('favoriteMovies');
     if (favoritesJson != null) {
       final List<dynamic> favoritesList = jsonDecode(favoritesJson);
       _favoriteMovies = favoritesList.map((json) => Movie.fromJson(json)).toList();
-      notifyListeners();
+    } else {
+      _favoriteMovies = [];
     }
+    notifyListeners();
   }
 
-  void _saveFavorites() async {
+  Future<void> _saveFavorites() async {
     final prefs = await SharedPreferences.getInstance();
     final String favoritesJson = jsonEncode(_favoriteMovies.map((movie) => movie.toJson()).toList());
     await prefs.setString('favoriteMovies', favoritesJson);
@@ -35,7 +49,7 @@ class FavoritesProvider with ChangeNotifier {
     } else {
       _favoriteMovies.add(movie);
     }
-    _saveFavorites(); // Save the updated list
+    _saveFavorites();
     notifyListeners();
   }
 }
